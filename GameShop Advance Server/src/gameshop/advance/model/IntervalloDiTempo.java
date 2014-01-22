2bd82dbc9c7c57d8a670aa279abd1809206cab6f
darkwiz <ilbaroncino@gmail.com>
2014-01-22 12:40:09 +0100

    Intervallo di tempo creato

--- GameShop Advance Server/src/gameshop/advance/model/IntervalloDiTempo.java
+++ GameShop Advance Server/src/gameshop/advance/model/IntervalloDiTempo.java
@@ -5,11 +5,29 @@
  */
 
 package gameshop.advance.model;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Interval;
+import org.joda.time.Months;
 
 /**
  *
- * @author Lorenzo Di Giuseppe <lorenzo.digiuseppe88@gmail.com>
+ * @author matteog
  */
 public class IntervalloDiTempo {
+    
+     DateTime startDate = new DateTime();
+     DateTime endDate = startDate.plus(Months.months(2));
+     
+     Interval interval = new Interval(startDate, endDate);
+        
+     public boolean isActual(){
+         Duration duration = this.interval.toDuration();
+         if(duration.equals(0))
+             return false;
+         else
+             return true;
+     }
+    
     
 }
