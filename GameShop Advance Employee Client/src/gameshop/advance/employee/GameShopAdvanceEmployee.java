32b1717e17c8f0b15b978f360bca977e89a4d15e
darkwiz <ilbaroncino@gmail.com>
2014-02-02 11:54:24 +0100

    Creato script InitializeConfiguration

--- GameShop Advance Employee Client/src/gameshop/advance/employee/GameShopAdvanceEmployee.java
+++ GameShop Advance Employee Client/src/gameshop/advance/employee/GameShopAdvanceEmployee.java
@@ -1,6 +1,5 @@
 package gameshop.advance.employee;
 
-import gameshop.advance.config.ConfigurationControllerSingleton;
 import gameshop.advance.exceptions.ConfigurationException;
 import gameshop.advance.ui.swing.UIWindowSingleton;
 import gameshop.advance.ui.swing.employee.EmployeeMenuPanel;
@@ -28,9 +27,6 @@ public class GameShopAdvanceEmployee {
                     //controllerConfig.getConfiguration();
                 }
             }
-            
-            ConfigurationControllerSingleton controllerConfig = ConfigurationControllerSingleton.getInstance();
-            controllerConfig.setConfiguration("127.0.0.1", 1099, 17);
             
             UIWindowSingleton window = UIWindowSingleton.getInstance();
             window.setPanel(new EmployeeMenuPanel());
