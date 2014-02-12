ff0e8a661280ad080f51ca52051d9d341e309c85
Loki88 <laplacetransformed@gmail.com>
2014-02-12 09:44:06 +0100

    Creata struttura controller di caso d'uso. To-do: -sistemare logica dei decorator -sistemare terminale cliente -sistemare terminale commesso -implementare controllore del caso d'uso PrenotaProdotto -sistemare la logica di acquisto, includendo la verifica della quantità dei prodotti

---
+++ GameShop Advance Server/src/gameshop/advance/controller/PrenotaProdottoController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameshop.advance.controller;
+
+import gameshop.advance.utility.IDProdotto;
+import gameshop.advance.utility.Money;
+
+/**
+ *
+ * @author Lorenzo Di Giuseppe <lorenzo.digiuseppe88@gmail.com>
+ */
+public class PrenotaProdottoController {
+    
+    //OPERAZIONI DI SISTEMA LATO CLIENTE
+    
+    public void avviaPrenotazione()
+    {
+        
+    }
+    
+    public void prenotaProdotto(IDProdotto codiceProdotto, int quantity)
+    {
+        
+    }
+    
+    public void terminaPrenotazione()
+    {
+        
+    }
+    
+    //OPERAZIONI DI SISTEMA LATO COMMESSO
+    
+    public void ricercaPrenotazione()
+    {
+        
+    }
+    
+    public void recuperaPrenotazione(Integer id)
+    {
+        
+    }
+    
+    public void gestisciPagamento(Money amount)
+    {
+        
+    }
+    
+}
