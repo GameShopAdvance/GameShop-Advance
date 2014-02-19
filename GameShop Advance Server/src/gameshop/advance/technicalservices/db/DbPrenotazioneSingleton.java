90add7ae035aad4320252e30dd97643fee641286
Loki88 <laplacetransformed@gmail.com>
2014-02-19 18:37:21 +0100

    Db con proxy

--- GameShop Advance Server/src/gameshop/advance/technicalservices/db/DbPrenotazioneSingleton.java
+++ GameShop Advance Server/src/gameshop/advance/technicalservices/db/DbPrenotazioneSingleton.java
@@ -8,9 +8,11 @@ package gameshop.advance.technicalservices.db;
 
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
-import com.db4o.query.Query;
+import com.db4o.query.Predicate;
 import gameshop.advance.exceptions.ObjectAlreadyExistsDbException;
 import gameshop.advance.interfaces.IPrenotazione;
+import gameshop.advance.model.transazione.proxies.PrenotazioneSmartProxy;
+import java.rmi.RemoteException;
 
 /**
  *
@@ -37,24 +39,36 @@ public class DbPrenotazioneSingleton {
         int exist = client.queryByExample(book).size();
         if(exist > 0)
             throw new ObjectAlreadyExistsDbException();
-        client.store(SmartProxyFactorySingleton.getIstance().creaProxyPrenotazione(book));
+        client.store(new PrenotazioneSmartProxy(book));
         client.commit();
     }
     
-     public IPrenotazione read(Integer id){
+     public IPrenotazione read(final Integer id)
+     {
         ObjectContainer client = DbManagerSingleton.getInstance().getClient();
-        Query query=client.query();
-        query.constrain(IPrenotazione.class);
-        query.descend("codiceProdotto").constrain(id);
-        ObjectSet results = query.execute();
-        if(results.isEmpty())
+        ObjectSet<PrenotazioneSmartProxy> result = client.query(new Predicate<PrenotazioneSmartProxy>() {
+            @Override
+            public boolean match(PrenotazioneSmartProxy candidate) {
+                Integer idTrans;
+                try {
+                    idTrans = candidate.getId();
+                }
+                catch (RemoteException ex) {
+                    return false;
+                }
+                if(idTrans.intValue() == id.intValue())
+                    return true;
+                else
+                    return false;
+            }
+        });
+        if(result.isEmpty())
             return null;
-        return (IPrenotazione) results.get(0);
+        return (IPrenotazione) result.get(0);
     }
      
      public Integer count(){
         ObjectContainer client = DbManagerSingleton.getInstance().getClient();
-        IPrenotazione pren = SmartProxyFactorySingleton.getIstance().creaProxyPrenotazione(null);
-        return client.queryByExample(pren.getClass()).size();
+        return client.queryByExample(PrenotazioneSmartProxy.class).size();
     }
 }
