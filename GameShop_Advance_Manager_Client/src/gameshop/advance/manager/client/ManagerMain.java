cc37c38226274dcf9360fe00865bb668f95e6794
Loki88 <laplacetransformed@gmail.com>
2014-04-02 20:16:35 +0200

    Menu manager

---
+++ GameShop_Advance_Manager_Client/src/gameshop/advance/manager/client/ManagerMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameshop.advance.manager.client;
+
+/**
+ *
+ * @author Lorenzo Di Giuseppe <lorenzo.digiuseppe88@gmail.com>
+ */
+public class ManagerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
