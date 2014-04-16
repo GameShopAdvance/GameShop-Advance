438bd32713db8184539ab1062ddaf2d8bbab45ee
Loki88 <laplacetransformed@gmail.com>
2014-04-16 14:05:36 +0200

    Proxy delle immagini

---
+++ GameShop Advance Server/src/gameshop/advance/remote/ImageProxy.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameshop.advance.remote;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Lorenzo Di Giuseppe <lorenzo.digiuseppe88@gmail.com>
+ */
+public class ImageProxy extends UnicastRemoteObject implements IRemoteImage {
+    
+    private static String imageDir = "./img/";
+    private final String imagePath;
+    
+    public ImageProxy(String path) throws RemoteException {
+        this.imagePath = path;
+    }
+
+    @Override
+    public ImageIcon getIcon() throws RemoteException {
+        String fullPath = imageDir.concat("small/").concat(this.imagePath);
+        return new ImageIcon(fullPath);
+    }
+
+    @Override
+    public ImageIcon getImage() throws RemoteException {
+        String fullPath = imageDir.concat("regular/").concat(this.imagePath);
+        return new ImageIcon(fullPath);
+    }
+}
